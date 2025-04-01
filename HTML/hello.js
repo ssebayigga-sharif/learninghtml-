3d569d451a7100ccd3634ec7bd366857e971eaf5
ssebayigga-sharif <sharifsseba@gmail.com>
2025-04-01 10:25:00 +0300

    innitial commit

---
+++ HTML/hello.js
@@ -0,0 +1,12 @@
+document.getElementById("ka").innerHTML=('i always dream of being a good programmer')
+document.getElementById('ka').style.textAlign=('center')
+document.getElementById('ka').style.color=('green')
+document.getElementById('ka').style.fontStyle=('bold')
+document.getElementById('ka').style.fontFamily=('courier')
+let a,b,c,d;
+a=5;
+b=3;
+c=7;
+d=a+b*c;
+document.getElementById('hi').innerHTML=d;
+document.getElementById('hi').style.textAlign=('center')
---
+++ HTML/index.js
@@ -0,0 +1,14 @@
+document.getElementById("demo").innerHTML="he lives in kayunga."
+document.getElementById("uu").innerHTML="hello javascript"
+document.getElementById("ii").innerHTML="its a beautiful town"
+document.getElementById("ii").style.color="red"
+document.getElementById("uu").style.fontStyle="italic"
+document.getElementById("demo").style.Color="green"
+window.alert(4+4 );
+document.write(5+7);
+console.log("hello world");
+let x=2;
+let y=16;
+let z=y*x;
+document.getElementById(h).innerHTML=( z);
+
